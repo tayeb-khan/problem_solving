89846db8cf9af302c1a892d72548af44636825c0
Tayef <tayeb.khan320@gmail.com>
2023-08-19 23:13:23 +0600

    33_search_in_rotated_sorted_array

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 These Repo is all about solving the problems from the <a href="https://leetcode.com">LeetCode</a> <br/>
-My LeetCode Profile: <a href="https://leetcode.com/tayeb320">https://leetcode.com/tayeb320 </a>
+My LeetCode Profile: <a href="https://leetcode.com/tayeb-khan">https://leetcode.com/tayeb-khan </a>
--- array/33_search in_rotated_sorted_array/index.php
+++ array/33_search in_rotated_sorted_array/index.php
@@ -8,6 +8,21 @@ class Solution {
      * @return Integer
      */
     function search($nums, $target) {
+        $nums = [4,5,6,7,0,1,2];
+        $target = 0;
 
+        $total_numbers = count($nums);
+
+        $median = ceil($total_numbers / 2);
+
+        $left = 0;
+
+        if ($nums[$median] >= $target) {
+            $left = $median;
+        } else {
+            $left = $median + 1;
+        }
+
+        dd($nums[$left]);
     }
 }
